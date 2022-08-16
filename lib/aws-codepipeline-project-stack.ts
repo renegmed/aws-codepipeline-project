980ad0ff353783d5ec4d34df1e8a137ac2bb1e5d
Rene Medalla <renemed@pop-os.localdomain>
2022-08-16 16:03:28 -0400

    initial commit

--- lib/aws-codepipeline-project-stack.ts
+++ lib/aws-codepipeline-project-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class AwsCodepipelineProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: "CDKTestPipeline", // Creating a new code pipeline which is a contstruct 
+      synth: new ShellStep('Synth', {  // Add a new synthesis 'shellstep' which will be pointed at our github repository
+        input: CodePipelineSource.gitHub('renegmed/aws-codepipeline-project', 'master'), // replace the GithHub repository 
 
-    // The code that defines your stack goes here
+          // The build steps for the pipeline are defined by these commands
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCodepipelineProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+          commands: [ 'npm ci',
+                      'npm run build',
+                      'npx cdk synth']
+      }),
+    });
+
+    /*
+      npm ci - (npm clean install), which is similar to npm install that is to be used in 
+              automated environments.
+      npm run build - to allow us to perform any necessary building/prep tasks for the project.
+      npx cdk synth - to synthesize whatever we have in the cloud formation stack to generate 
+              the self mutating pipeline.
+    */
   }
 }
